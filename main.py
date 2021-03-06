35ed70bdb5ccd451d3236aecb18f3c299373e912
Olivia <olga.murzina@inbox.ru>
2021-03-06 22:47:35 +0300

    Игра 2048. Пробую экран заставки и аварийный выход

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # v2_classic branch
 import random
+import sys
 
 import pygame
 
-# частота смены кадров в 1 секунду
-FPS = 50
+# частота смены кадров в 10 секунду
+FPS = 10
 
 
 class Board:
@@ -268,6 +269,39 @@ class Board:
                                         self.board[x][y] = 0
             print(self.score)
 
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
 
 def main():
     pygame.init()
